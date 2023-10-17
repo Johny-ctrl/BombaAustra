de1a61fd914dd90acc763393067fa232410e3f41
johny-ctrl <john.carvajal@duocuc.cl>
2023-10-17 18:43:00 -0300

    Creacion de Entidades

---
+++ BombaAustra.Shared/Entities/NombreCompañia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombaAustra.Shared.Entities
+{
+    internal class NombreCompañia
+    {
+    }
+}
---
+++ BombaAustra.Shared/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombaAustra.Shared.Entities
+{
+    internal class Usuario
+    {
+        public string ID_RUT { get; set; } = null!;
+    }
+}
