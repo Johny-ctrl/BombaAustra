bdbf2ebaec1d3db3e7d791e8705dc9700185e9fc
johny-ctrl <john.carvajal@duocuc.cl>
2023-10-19 17:52:38 -0300

    DataContext corregido

--- BombaAustra.API/Data/DataContext.cs
+++ BombaAustra.API/Data/DataContext.cs
@@ -11,27 +11,50 @@ namespace BombaAustra.API.Data
         {
         }
 
-        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Usuario> USUARIOS { get; set; }
 
-        public DbSet<TipoUsuario> tipo_Usuarios { get; set; }
+        public DbSet<TipoUsuario> TIPO_USUARIOS { get; set; }
 
-        public DbSet<TipoEquipo> tipo_Equipos {  get; set; }
+        public DbSet<TipoEquipo> TIPO_EQUIPO {  get; set; }
 
-        public DbSet<Reporte> reportes { get; set; }
+        public DbSet<Reporte> REPORTES { get; set; }
 
-        public DbSet<NombreCompañia> nombre_Compañia { get; set; }
+        public DbSet<NombreCompañia> NOMBRE_COMPANIAS { get; set; }
 
-        public DbSet<EstadoEquipo> estado_Equipos { get; set; }
+        public DbSet<EstadoEquipo> ESTADO_EQUIPO { get; set; }
 
-        public DbSet<Equipo> equipos { get; set; }
+        public DbSet<Equipo> EQUIPOS { get; set; }
 
-        public DbSet<Compañia> compañias { get; set; }
+        public DbSet<Compañia> COMPANIAS { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Usuario>().HasIndex(x => x.ID_RUT).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TipoUsuario>().HasIndex(x => x.ID_TIPO_USUARIO).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TipoEquipo>().HasIndex(x => x.ID_TIPO_EQUIPO).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Reporte>().HasIndex(x => x.ID_REPORTE).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NombreCompañia>().HasIndex(x => x.ID_NOMBRE_COMPAÑIA).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<EstadoEquipo>().HasIndex(x => x.ID_ESTADO).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Equipo>().HasIndex(x => x.ID_EQUIPO).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Compañia>().HasIndex(x => x.ID_COMPAÑIA).IsUnique();
+
+
         }
     }
 }
