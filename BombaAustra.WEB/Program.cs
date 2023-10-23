85d5682603c2bf0b3f7ff0cec95761434ddd2e0a
johny-ctrl <john.carvajal@duocuc.cl>
2023-10-22 22:01:31 -0300

    Logro obtenido! pc por la ventana (tambien avanzo de form)

--- BombaAustra.WEB/Program.cs
+++ BombaAustra.WEB/Program.cs
@@ -1,5 +1,6 @@
 using BombaAustra.WEB;
 using BombaAustra.WEB.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +12,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https:/
 
 //existen 3 formas de inyectar repositorio
 builder.Services.AddScoped<IRepository, Repository>();//Implementa el repositorio, esta es la mas comun, Se usa cuando se quiere crear una nueva instancia
+builder.Services.AddSweetAlert2(); //sweet Alert 2
 
 //builder.Services.AddTransient<IRepository, Repository>(); se usa cuando se quiere implementar solo 1 vez
 //builder.Services.AddSingleton<>; SON MUY PELIGROSOS, consumen muchos recursos, se quedan dando vueltas los objetos que usan y pueden generar brechas de seguridad, inseguros por naturaleza
