dc85a00c6a019a2f7aef2502192c3ddbcd313469
Johny-ctrl <john.carvajal@duocuc.cl>
2023-11-28 14:44:39 -0300

    si

--- BombaAustra.WEB/Repositories/IRepository.cs
+++ BombaAustra.WEB/Repositories/IRepository.cs
@@ -3,19 +3,19 @@
     //Inyeccion de la interfaz
     public interface IRepository
     {
-        Task<HttpResponseWrapper<T>> Get<T>(string url);//<--Devuelve metodo de T
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
 
-        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);//<-- Post que no devuelve nada
+        Task<HttpResponseWrapper<object>> Get(string url);
 
-        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);//<-- Post que devuelve algo
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
 
         Task<HttpResponseWrapper<object>> Delete(string url);
 
         Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
 
         Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
-
-        Task<HttpResponseWrapper<object>> Get(string url);
 
     }
 
